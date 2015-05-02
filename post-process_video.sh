14c8e53d47438d1cebc23e3fda101271bf62a1bb
wurmfood <wurmfood@gmail.com>
2015-05-01 21:04:11 -0700

    Fixing the script actually make it work. Not at all how I want, but it works.

--- post-process_video.sh
+++ post-process_video.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
 # Dir we're going to get video from
-VIDEO_DIR = $1
-OUT_FILE = "./output.mp4"
+VIDEO_DIR=$1
+OUT_FILE="./output.mp4"
 
 if [ -f $2 ]; then
-    OUT_FILE = $2
+    $OUT_FILE = $2
 fi
 
 
 # First argument should be a directory. Make sure it is.
 if [ -d $VIDEO_DIR ]; then
-    for f in $VIDEO_DIR/*.h264;
-        do echo "file '$f'" >> /tmp/video_file.list
+    cd $VIDEO_DIR
+    for f in ./*.h264; do
+        echo "file '$f'" >> video_file.list
+        echo $f
     done
 
-    ffmpeg -f concat -i /tmp/video_file.list -c copy /tmp/output.h264
-    avconv -r 30 -i /tmp/output.h264 -vcodec copy $OUT_FILE
+    ffmpeg -f concat -i video_file.list -c copy output.h264
+    avconv -r 30 -i output.h264 -vcodec copy $OUT_FILE
+    rm outfile.h264
+    rm video_file.list
 else
     echo "Invalid directory. Please pass a directory to the script first."
 fi
