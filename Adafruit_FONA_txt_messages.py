15de50b2e4faa71d2c574e4e0070b2fc6347fbc5
wurmfood <wurmfood@gmail.com>
2015-06-27 13:33:46 -0700

    Adding a text message only module. Otherwise this is taking too long.

--- Adafruit_FONA_txt_messages.py
+++ Adafruit_FONA_txt_messages.py
@@ -11,6 +11,8 @@
 #   PS pin is in input mode. Should use a resistor to force it up or down to start.
 #   For reset and key pins,  you need additional hardware to do anything useful.
 #
+# While I wanted to do something more comprehensive, I'm just doing the text messaging part for now.
+#
 #####################################################################
 
 #################
@@ -22,6 +24,26 @@ except:
     pass
 import serial
 
+
+#################
+# Text message object
+#################
+class Fona_message(object):
+    def __init__(self, raw_text_message):
+        self.__sender_number, self.__text_message = self.__parse(raw_text_message)
+
+    @staticmethod
+    def __parse(self, raw_text_message):
+        # These are fake numbers for the moment. TODO: Get the real numbers in here.
+        return raw_text_message[0:8], raw_text_message[9:]
+
+    @property
+    def sender_number(self):
+        return self.__sender_number
+
+    @property
+    def text_message(self):
+        return self.__text_message
 
 #################
 # FONA object
@@ -89,6 +111,11 @@ class Fona(object):
                                         send_message="AT+CMGS"
                                         )
 
+        # Finally, some commands we're going to do simply because all we care about is text messaging
+        self.__set_value(self.__text_msg_commands['text_message_format'], 1)
+        self.__set_value(self.__set_commands['ringer'], 1)
+        self.__set_value(self.__set_commands['use_local_timestamp'], 1)
+
     def __status_query(self, cmd):
         """
 
@@ -140,18 +167,6 @@ class Fona(object):
     def connected(self):
         return self.__connected
 
-    def set_fona_option(self, key, value):
-        if key in self.__set_commands:
-            if self.__connected:
-                self.__set_value(self.__set_commands[key], value)
-            else:
-                raise serial.SerialException("Not connected to FONA. Can't set option.")
-        else:
-            classname = self.__class__.__name__
-            raise AttributeError("'{classname}' object has no "
-                                 "attribute '{item}'".format(**locals()))
-        pass
-
     def list_current_text_messages(self, include_read=False, leave_unread=False):
         if self.__connected:
             # Make sure we're in the correct format:
@@ -172,7 +187,27 @@ class Fona(object):
             raise serial.SerialException("Not connected to FONA. Can't read text messages.")
 
     def send_text_message(self, destination_number, message):
-        pass
+        if len(message) > 140:
+            print("Message too long. Aborting.")
+            return 0
+        command = ["AT+CMGS=\"",    # send message command
+                   destination_number,     # Destination phone number
+                   "\"",            # Close the quote on the number
+                   "\n",            # Newline
+                   message,         # The actual message
+                   "\x1A"]          # A Ctrl-Z to end.
+
+        try:
+            self.__my_port.write("".join(command).encode("ascii"))
+
+            # Print out the response
+            response = []
+            for line in self.__my_port:
+                response.append(line.rstrip().decode("ascii"))
+            return response
+        except serial.SerialException as err:
+            print(err.args[0])
+            return 0
 
     def set_callback_on_pin(self, callback_function, pin):
         """
@@ -193,7 +228,8 @@ class Fona(object):
 
 # Only for testing. Remove later.
 SERIAL_PORT = "/dev/ttyUSB0"
-GPIO.setmode(GPIO.BCM)
+if GPIO:
+    GPIO.setmode(GPIO.BCM)
 my_fona = Fona(SERIAL_PORT)
 my_fona.connect()
 
@@ -205,3 +241,5 @@ print(my_fona.ringer)
 print(my_fona.set_fona_option("error_verbosity", 2))
 print(my_fona.list_current_text_messages())
 # GPIO.cleanup()
+print(my_fona.send_text_message(4158284862, "This is a test message."))
+print(my_fona.list_current_text_messages(include_read=True, leave_unread=True))
